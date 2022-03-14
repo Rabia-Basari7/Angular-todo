11164ce9d0a160ff16fd6f7c3d92a4a0775e766b
Rabia-Basari777 <rabia_fw10_200@masai.school>
2022-03-14 10:50:36 +0530

    First Angular App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-angular-todo';
+  title = 'todo-app';
+
+  nextId = 4;
+  todos = [
+    {
+      id: 1,
+      label: 'Learn Angular',
+      done: false
+    },
+    {
+      id: 2,
+      label: 'Make projects',
+      done: false
+    },
+    {
+      id: 3,
+      label: 'Attend Scrum',
+      done: true
+    },
+  ];
+
+  delete(todo) {
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+  }
+
+  add(newTodoLabel) {
+    let newTodo = {
+      id: this.nextId,
+      label: newTodoLabel,
+      done: false
+    };
+    this.todos.push(newTodo);
+    this.nextId++;
+  }
+
+  toggle(todo) {
+    todo.done = !todo.done;
+  }
 }
